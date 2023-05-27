3b2106416d95f03695f93e86205861480c06bbf0
Degreco <antoniodaniel@estudiante.edib.es>
2023-05-27 19:05:42 +0200

    Refactor userRole

--- ChickenAndTurkey/src/userRole.java
+++ ChickenAndTurkey/src/userRole.java
@@ -1,18 +1,15 @@
 public class userRole {
     private int userType;
+    
+    private static final int ADMINISTRATOR = 1;
+    private static final int ADMINISTRATIVO = 2;
 
-    public userRole(int userType) {
-        this.userType = userType;
-    }
+
 
     public void showMenu() {
-        // Tipos de usuario
-        // 1 = Administrador
-        // 2 = Administrativo
-        if (userType == 1) {
-            // Menu administrador
+        if (userType == ADMINISTRATOR) {
             menuAdministrador.mostrarMenu();
-        } else if (userType == 2) {
+        } else if (userType == ADMINISTRATIVO) {
             menuAdministrativo.mostrarMenu();
         } else {
             System.out.println("Tipo de usuario incorrecto");
