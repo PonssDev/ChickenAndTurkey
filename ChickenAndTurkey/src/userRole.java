9576dd0fd38c55c72d49c8f60f5a3e7fdd271ed9
Pons <alanponstrabajo@gmail.com>
2023-05-29 19:48:32 +0200

    Solucionar bug userType [Daniel]

--- ChickenAndTurkey/src/userRole.java
+++ ChickenAndTurkey/src/userRole.java
@@ -4,7 +4,9 @@ public class userRole {
     private static final int ADMINISTRATOR = 1;
     private static final int ADMINISTRATIVO = 2;
 
-
+    public userRole(int userType) {
+        this.userType = userType;
+    }
 
     public void showMenu() {
         if (userType == ADMINISTRATOR) {
