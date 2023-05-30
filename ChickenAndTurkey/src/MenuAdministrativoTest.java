adce6d1f74f4c8acafd2741f0825dc0ce781a466
Degreco <antoniodaniel@estudiante.edib.es>
2023-05-30 20:11:04 +0200

    Tests clase MenuAdministrativo

---
+++ ChickenAndTurkey/src/MenuAdministrativoTest.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MenuAdministrativoTest {
+
+    @Test
+    public void testConsultarUsuarioPorDNI() {
+        // Arrange
+        String dniExistente = "45185631S";
+        String dniNoExistente = "00000000X";
+
+        // Act
+        boolean usuarioExistente = menuAdministrativo.consultarUsuarioPorDNI(dniExistente);
+        boolean usuarioNoExistente = menuAdministrativo.consultarUsuarioPorDNI(dniNoExistente);
+
+        // Assert
+        Assert.assertTrue(usuarioExistente);
+        Assert.assertFalse(usuarioNoExistente);
+    }
+
+    @Test
+    public void testBuscarVehiculoPorMatricula() {
+        // Arrange
+        String matriculaExistente = "451851651s";
+        String matriculaNoExistente = "XYZ789876f";
+
+        // Act
+        boolean vehiculoExistente = menuAdministrativo.buscarVehiculoPorMatricula(matriculaExistente);
+        System.out.println(vehiculoExistente);
+        boolean vehiculoNoExistente = menuAdministrativo.buscarVehiculoPorMatricula(matriculaNoExistente);
+
+        // Assert
+        Assert.assertTrue(vehiculoExistente);
+        Assert.assertFalse(vehiculoNoExistente);
+    }
+}
